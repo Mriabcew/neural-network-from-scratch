7c5582db77343136983b4ea9e8afc914f6a076c5
Mriabcew <92296396+Mriabcew@users.noreply.github.com>
2026-06-09 20:28:29 +0200

    feat: dense layer with forward and backward pass

---
+++ src/layers.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = np.random.randn(n_inputs, n_neurons) * 0.01
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, x):
+        self.x = x
+        return np.dot(x, self.weights) + self.biases
+
+    def backward(self, dout):
+        self.dweights = np.dot(self.x.T, dout)
+        self.dbiases = np.sum(dout, axis=0, keepdims=True)
+        return np.dot(dout, self.weights.T)
